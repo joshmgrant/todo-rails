3387fef79df98bbbb3d14d2b5c0471b03bf706f5
Josh Grant <joshua.m.grant@gmail.com>
2024-05-30 23:01:13 -0400

    initial work

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,31 @@
+class TodosController < ApplicationController
+  def index
+    todos = Todo.order("created_at DESC")
+    render json: todos
+  end
+
+  def create
+    todo = Todo.create(todo_param)
+
+    render json: todo, status: :created
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_param)
+
+    render json: todo, status: :ok
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+
+    head :no_content, status: :no_content
+  end
+
+  private 
+  def todo_param
+    params.require(:todo).permit(:title, :done)
+  end
+end
