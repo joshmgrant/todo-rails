9c3e324cfdf7353514a3ef99afc94b895a5aa688
Josh Grant <joshua.m.grant@gmail.com>
2024-06-14 10:25:08 -0400

    starting out

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   scope '/api/v1' do
     resources :todos
+    resources :rando
+    resources :randos
   end
 
 
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -2,22 +2,35 @@ require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get todos_index_url
+    get todos_url
     assert_response :success
   end
 
-  test "should get create" do
-    get todos_create_url
-    assert_response :success
+  test "should create todos" do
+    assert_difference("Todo.count") do
+      post todos_url, params: { todo: { done: false, title: "Hello Rails" } }
+    end
+  
+    assert_response :created
   end
 
-  test "should get update" do
-    get todos_update_url
-    assert_response :success
+  test "should update todo" do
+    todo = todos(:one)
+   
+    patch todo_url(todo), params: { todo: { title: "Learning Rails" } }
+   
+    # no frontend or form here to manage
+    # Reload association to fetch updated data and assert that title is updated.
+    todo.reload
+    assert_equal "Learning Rails", todo.title
   end
 
-  test "should get destroy" do
-    get todos_destroy_url
-    assert_response :success
+  test "should destroy todo" do
+    todo = todos(:one)
+    assert_difference("Todo.count", -1) do
+      delete todo_url(todo)
+    end
+  
+    assert_response :no_content
   end
 end
