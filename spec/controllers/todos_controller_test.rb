928e99115b0264d9d0539c9748d2d898ba0d894b
Josh Grant <joshua.m.grant@gmail.com>
2024-08-06 09:44:42 -0400

    added get show case

--- spec/controllers/todos_controller_test.rb
+++ spec/controllers/todos_controller_test.rb
@@ -41,27 +41,15 @@ RSpec.describe TodosController, type: :controller do
       expect(response).to have_http_status(:no_content)
     end
   end
-
-  # test "should destroy todo" do
-  #   todo = create(:todo)
-  #   # assert_difference("Todo.count", -1) do
-  #   #   delete todo_url(todo)
-  #   # end
-    
-  #   assert_raises(ActiveRecord::RecordNotFound) do
-  #     delete todo_url(todo)
-  #     todo.reload
-  #     todo
-  #   end
   
-  #   assert_response :no_content
-  # end
+  describe 'GET show' do
+    let(:todo) { create(:todo) }
 
-  # test "should show todo" do
-  #   todo = todos(:one)
-  #   get todo_url(todo)
-  #   assert_response :success
-  # end
+    it 'should show one todo' do
+      get :show, params: { id: todo.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
   # test "should return not found for non-existing todo" do
   #   get todo_url(id: 'non-existing-id')
